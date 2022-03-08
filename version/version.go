a549e3bd0582a7dd48f4dff51b5315ed283077bc
mbiciunas@gmail.com <73kObaAq3E4R>
2022-03-08 16:46:40 -0500

    Not quite working

--- version/version.go
+++ version/version.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-	_ "embed"
+	"embed"
+	"io/fs"
 	"fmt"
-	"strings"
+	"log"
+	"net/http"
+	"os"
 )
 
-var (
-	Version string = strings.TrimSpace(version)
-	//go:embed version.txt
-	version string
-)
+//go:embed static
+var embededFiles embed.FS
 
 func main() {
-	fmt.Printf("Version %q\n", Version)
+	// using the function
+	mydir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("*************************************")
+	fmt.Println(mydir)
+	fmt.Println("*************************************")
+
+	directory := http.Dir("./static")
+	fmt.Printf(string(directory))
+	fileServer := http.FileServer(http.Dir(files)) // New code
+	//fileServer := http.FileServer(http.Dir("./static")) // New code
+	http.Handle("/", fileServer) // New code
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+# https://blog.carlmjohnson.net/post/2021/how-to-use-go-embed/
+
+
+
+func getFileSystem() http.FileSystem {
+	fsys, err := fs.Sub(embededFiles, "static")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
 }
