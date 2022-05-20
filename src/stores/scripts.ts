2bdbd2b749495482ac50aa59166625ebd661a37c
mbiciunas@gmail.com <73kObaAq3E4R>
2022-05-20 16:59:30 -0400

    Fix fetchscripts to use proper type.

--- src/stores/scripts.ts
+++ src/stores/scripts.ts
@@ -1,10 +1,10 @@
 import { defineStore } from 'pinia'
 import axios from "axios"
 
-export type Script = { Id: number; Name: string; Description: string; Code: string }
+export type Script = { id: number; Name: string; Description: string; Code: string }
 
 export const useScriptStore = defineStore({
-    id: "script",
+    id: "scripts",
     state: () => ({
         scripts: [] as Script[],
     }),
@@ -15,24 +15,22 @@ export const useScriptStore = defineStore({
         getScriptById: (state) => {
             return (scriptId: number) => state.scripts.find((script) => script.Id === scriptId)
         },
-
-        // getScript(state, id: number){
-        //     return state.scripts[id]
-        // }
     },
     actions: {
         async fetchScripts() {
             try {
                 const data = await axios.get('http://localhost:8080/api/scripts')
-                // Remove everything from this.scripts.
-                // Build Script type with data from data...
-                // Push into this.scripts.
-                // Use map to traverse data and arrow function to transform data into type Script.
+                console.log("Original scripts", this.scripts)
+                console.log("Raw data", data.data)
+                this.scripts = data.data.map(data => {
+                    console.log("id: " + data.Id, "name: " + data.Name)
+                    const newScript: Script = {id: data.Id, Name: data.Name, Description: data.Description, Code: data.Code}
+                    console.log("newScript", newScript)
+                    return newScript
+                })
 
-                // Add type to axios.get<???>
-                // May need @types/axios plugin as developer dependency.
-                this.scripts = data.data
-                console.log(this.scripts)
+                // this.scripts = data.data
+                console.log("new scripts", this.scripts)
             }
             catch (error) {
                 alert(error)
@@ -47,15 +45,8 @@ export const useScriptStore = defineStore({
             }
 
             try {
-                const data = await axios.post('http://localhost:8080/api/scripts', post)
+                await axios.post('http://localhost:8080/api/scripts', post)
 
-                // const newScript: Script = {Id: data.data, Name: name, Description: description, Code: code}
-
-                // this.scripts.push(newScript)
-
-                //
-                // Rebuild store from fetch
-                //
                 await this.fetchScripts()
             }
             catch (error) {
@@ -65,11 +56,8 @@ export const useScriptStore = defineStore({
         },
         async deleteScript(id : number) {
             try {
-                const data = await axios.delete('http://localhost:8080/api/scripts/' + id)
+                await axios.delete('http://localhost:8080/api/scripts/' + id)
 
-                //
-                // Rebuild store from fetch
-                //
                 await this.fetchScripts()
             }
             catch (error) {
@@ -79,7 +67,7 @@ export const useScriptStore = defineStore({
         },
         async runScript(id : number) {
             try {
-                const data = await axios.post('http://localhost:8080/api/scripts/' + id + '/run')
+                await axios.post('http://localhost:8080/api/scripts/' + id + '/run')
             }
             catch (error) {
                 alert(error)
