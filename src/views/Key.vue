a61ed2e692cb12068091a0f833a77492b1d4b278
mbiciunas@gmail.com <73kObaAq3E4R>
2022-09-28 11:10:19 -0400

    Missed in last commit...

---
+++ src/views/Key.vue
@@ -0,0 +1,25 @@
+<template>
+  <NavigationDrawer/>
+  <v-main>
+    <KeyGrid/>
+  </v-main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import KeyGrid from '@/components/key/KeyGrid.vue'
+import NavigationDrawer from '@/components/key/navigation/NavigationDrawer.vue'
+
+export default defineComponent({
+  components: {
+    NavigationDrawer,
+    KeyGrid,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
